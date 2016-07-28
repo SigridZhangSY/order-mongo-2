103b96acd2aef603aa261d717027127e8383f74b
SigridZhangSY <sigridzhang0603@163.com>
2016-07-28 20:28:32 +0800

    use jongo to create product

--- src/main/java/com/thoughtworks/ketsu/infrastructure/repository/ProductRepositoryImpl.java
+++ src/main/java/com/thoughtworks/ketsu/infrastructure/repository/ProductRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.ketsu.infrastructure.repository;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
 import com.thoughtworks.ketsu.domain.product.Product;
 import com.thoughtworks.ketsu.domain.product.ProductRepository;
 import org.bson.types.ObjectId;
+import org.jongo.Jongo;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Key;
 
@@ -14,8 +17,13 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Inject
     Datastore datastore;
 
+    @Inject
+    Jongo jongo;
+
     @Override
     public Optional<Product> createProduct(Map<String, Object> info) {
+
+        //use morphia save and find
         Product product = new Product(info.get("name").toString(),
                                     info.get("description").toString(),
                                     Double.valueOf(info.get("price").toString()));
