5a7694b78bbaa885e78136fa2025ea25fc7bfd1d
SigridZhangSY <sigridzhang0603@163.com>
2016-07-29 22:46:54 +0800

    return 400 when post order with name is empty

--- src/main/java/com/thoughtworks/ketsu/web/UserApi.java
+++ src/main/java/com/thoughtworks/ketsu/web/UserApi.java
@@ -3,6 +3,8 @@ package com.thoughtworks.ketsu.web;
 
 import com.thoughtworks.ketsu.domain.order.Order;
 import com.thoughtworks.ketsu.domain.user.User;
+import com.thoughtworks.ketsu.infrastructure.ParameterCheck;
+import com.thoughtworks.ketsu.web.exception.InvalidParameterException;
 import com.thoughtworks.ketsu.web.jersey.Routes;
 import org.bson.types.ObjectId;
 
@@ -10,6 +12,8 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UserApi {
@@ -31,7 +35,26 @@ public class UserApi {
     @Path("orders")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createOrder(Map<String, Object> info,
-                                @Context Routes routes){
+                                @Context Routes routes,
+                                @Context ParameterCheck parameterCheck){
+        List<String> fields = new ArrayList<>();
+        fields.add("name");
+        fields.add("address");
+        fields.add("phone");
+        fields.add("order_items");
+        List<String> empty = parameterCheck.emptyCheck(fields, info);
+        if(empty.size() > 0)
+            throw new InvalidParameterException(empty);
+        List<Map<String, Object>> items = (List)info.get("order_items");
+        for(Map<String, Object> item : items){
+            fields.clear();
+            empty.clear();
+            fields.add("product_id");
+            fields.add("quantity");
+            empty = parameterCheck.emptyCheck(fields, item);
+            if(empty.size() > 0)
+                throw new InvalidParameterException(empty);
+        }
         Order order = user.createOrder(info).get();
         System.out.println(order);
         System.out.println(routes);
--- src/test/java/com/thoughtworks/ketsu/web/UserApiTest.java
+++ src/test/java/com/thoughtworks/ketsu/web/UserApiTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -31,9 +32,23 @@ public class UserApiTest extends ApiSupport{
     public void should_return_201_and_uri__when_post_order(){
         User user = userRepository.createUser(TestHelper.userMap("xxx")).get();
         Product product = productRepository.createProduct(TestHelper.productMap("forOrder")).get();
+
         Response post = post("users/" + user.getId() + "/orders", TestHelper.orderMap(product.getId().toString()));
+
         assertThat(post.getStatus(), is(201));
         assertThat(Pattern.matches(".*/orders/.*", post.getLocation().toASCIIString()), is(true));
+    }
+
+    @Test
+    public void should_return_400_when_post_order_with_name_is_empty(){
+        User user = userRepository.createUser(TestHelper.userMap("xxx")).get();
+        Product product = productRepository.createProduct(TestHelper.productMap("forOrder")).get();
+        Map<String, Object> map = TestHelper.orderMap(product.getId().toString());
+        map.remove("name");
+
+        Response post = post("users/" + user.getId() + "/orders", map);
+
+        assertThat(post.getStatus(), is(400));
 
     }
 }
